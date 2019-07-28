580c51333a1cc758a545c714a6edaa14bc93f543
paggio <paggi96@outlook.com>
2019-07-28 12:46:50 +0200

    first version of classifier server

---
+++ server/server.py
@@ -0,0 +1,49 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import base64
+import io
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+from classifier import MLPClassifier, Fake
+import utils
+##CAN DO BETTER?
+image_classifier = MLPClassifier("adam_densnet121_fine_tuning.h5")
+#image_classifier = Fake()
+
+class ClassifierHttpServer(BaseHTTPRequestHandler):
+    def _set_response(self): #base header resposnse
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+
+    def do_POST(self): #post handler
+        print("reiceved...")
+        content_length = int(self.headers['Content-Length']) 
+        img_format = self.headers['Content-type'].split("/")[1] #take image format
+        post_data = self.rfile.read(content_length) #read image data
+        img = utils.load_from_bytes(post_data, img_format)
+        label = image_classifier.classify(img)
+        self._set_response()
+        self.wfile.write(json.dumps({"trashClass": label}).encode("utf-8"))
+        
+    def do_OPTIONS(self): #used due browser problem, first browser send OPTIONS to server!
+        self.send_response(204)
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Credentials", "true")
+        self.send_header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
+        self.send_header("Access-Control-Allow-Headers",
+                           "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+        print("options reiceved..")
+        self.end_headers()
+
+def run():
+    ##make parametrizable 
+    print('starting server...')
+    server_address = ('192.168.178.101', 8080) 
+    httpd = HTTPServer(server_address, ClassifierHttpServer)
+    print('running server...')
+    httpd.serve_forever()
+
+run()
